25ede5c087c6622ff40271410dfd94330c46e968
Seiji Sogabe <s.sogabe@gmail.com>
2015-08-25 10:38:09 +0900

    use Stopwatch of guava

--- src/test/java/com/buildria/restmock/TestNameRule.java
+++ src/test/java/com/buildria/restmock/TestNameRule.java
@@ -1,9 +1,12 @@
 package com.buildria.restmock;
 
+import com.google.common.base.Stopwatch;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static com.google.common.base.Stopwatch.createStarted;
 
 /**
  *
@@ -11,18 +14,18 @@ import org.slf4j.LoggerFactory;
  */
 public class TestNameRule extends TestWatcher {
 
-    private long start;
+    private Stopwatch sw;
 
     @Override
     protected void starting(Description description) {
-        start = System.currentTimeMillis();
+        sw = createStarted();
         log.info("=== {} starting ...", description.getMethodName());
     }
 
     @Override
     protected void finished(Description description) {
-        long end = System.currentTimeMillis();
-        log.info("=== {} finished. {} ms", description.getMethodName(), end - start);
+        sw.stop();
+        log.info("=== {} finished. {}", description.getMethodName(), sw);
     }
 
     private final Logger log = LoggerFactory.getLogger(TestNameRule.class);
