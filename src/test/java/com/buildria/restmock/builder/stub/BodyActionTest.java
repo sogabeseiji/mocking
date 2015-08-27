b4a68a077328a70d4861fdbbcf2a73306ce445d8
Seiji Sogabe <s.sogabe@gmail.com>
2015-08-27 23:02:34 +0900

    test Response configuref not to have Content-Type header.

--- src/test/java/com/buildria/restmock/builder/stub/BodyActionTest.java
+++ src/test/java/com/buildria/restmock/builder/stub/BodyActionTest.java
@@ -1,21 +1,28 @@
 package com.buildria.restmock.builder.stub;
 
+import com.buildria.restmock.RestMockException;
 import com.buildria.restmock.TestNameRule;
 import com.buildria.restmock.builder.stub.Action.BodyAction;
 import com.buildria.restmock.builder.stub.Action.HeaderAction;
+import com.buildria.restmock.serialize.ObjectSerializerContext;
 import com.buildria.restmock.serialize.Person;
 import com.buildria.restmock.stub.StubHttpServer;
 import com.google.common.base.MoreObjects;
+import com.google.common.net.MediaType;
+import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.DefaultHttpResponse;
 import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
+import java.nio.charset.StandardCharsets;
 import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
@@ -76,6 +83,28 @@ public class BodyActionTest {
 
         assertThat(out, notNullValue());
         assertThat(out.headers().get("Content-Length"), is("23"));
+
+        assertThat(out, instanceOf(DefaultFullHttpResponse.class));
+        DefaultFullHttpResponse response = (DefaultFullHttpResponse) out;
+        ByteBuf buf = response.content();
+        String json = buf.toString(StandardCharsets.UTF_8);
+
+        ObjectSerializerContext ctx
+                = new ObjectSerializerContext(person, MediaType.JSON_UTF_8.toString());
+        String expected = ObjectSerializerContext.JACKSON.serialize(ctx);
+
+        assertThat(json, is(expected));
+    }
+
+    @Test(expected = RestMockException.class)
+    public void testApplyResponseNoContentType() throws Exception {
+        StubHttpServer server = new StubHttpServer();
+        Matcher<?> uri = equalTo("/api/p");
+        Object content = person;
+
+        Action action = new BodyAction(server, uri, content);
+        HttpResponse in = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+        HttpResponse out = action.apply(in);
     }
 
     @Test
