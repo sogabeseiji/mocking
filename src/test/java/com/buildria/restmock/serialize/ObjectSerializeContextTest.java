29d3702fb9b3ac3c7123a6feb4c0a87a85178aa6
Seiji Sogabe <s.sogabe@gmail.com>
2015-08-27 16:21:51 +0900

    move Test methods to right class

---
+++ src/test/java/com/buildria/restmock/serialize/ObjectSerializeContextTest.java
@@ -0,0 +1,65 @@
+package com.buildria.restmock.serialize;
+
+import com.buildria.restmock.RestMockException;
+import com.buildria.restmock.TestNameRule;
+import com.google.common.net.MediaType;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class ObjectSerializeContextTest {
+
+    @Rule
+    public TestNameRule testNameRule = new TestNameRule();
+
+    @Test(expected = RestMockException.class)
+    public void testCreateInvalidContentType() {
+        Person person = new Person("Bob", 20);
+        ObjectSerializeContext ctx = new ObjectSerializeContext(person, MediaType.JPEG.toString());
+        ObjectSerializerFactory.create(ctx);
+    }
+
+    @Test
+    public void testCreateJAXB() {
+        Person person = new Person("Bob", 20);
+        ObjectSerializeContext ctx = new ObjectSerializeContext(person,
+                MediaType.XML_UTF_8.toString());
+        ObjectSerializer os = ObjectSerializerFactory.create(ctx);
+        assertThat(os, notNullValue());
+        assertThat(os, instanceOf(JAXBXmlSerializer.class));
+    }
+
+    @Test
+    public void testCreateJackson() {
+        Person person = new Person("Bob", 20);
+        ObjectSerializeContext ctx = new ObjectSerializeContext(person,
+                MediaType.JSON_UTF_8.toString());
+        ObjectSerializer os = ObjectSerializerFactory.create(ctx);
+        assertThat(os, notNullValue());
+        assertThat(os, instanceOf(JacksonJsonSerializer.class));
+    }
+
+    @Test
+    public void testCreateGson() {
+        Person person = new Person("Bob", 20);
+        ObjectSerializeContext ctx
+                = new ObjectSerializeContext(person, MediaType.JSON_UTF_8.toString()) {
+                    @Override
+                    protected boolean isGsonEnabled() {
+                        return true;
+                    }
+
+                    @Override
+                    protected boolean isJacksonEnabled() {
+                        return false;
+                    }
+                };
+        ObjectSerializer os = ObjectSerializerFactory.create(ctx);
+        assertThat(os, notNullValue());
+        assertThat(os, instanceOf(GsonJsonSerializer.class));
+    }
+
+}
--- src/test/java/com/buildria/restmock/serialize/ObjectSerializerFactoryTest.java
+++ src/test/java/com/buildria/restmock/serialize/ObjectSerializerFactoryTest.java
@@ -1,14 +1,8 @@
 package com.buildria.restmock.serialize;
 
-import com.buildria.restmock.RestMockException;
 import com.buildria.restmock.TestNameRule;
-import com.google.common.net.MediaType;
 import org.junit.Rule;
 import org.junit.Test;
-
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.Assert.assertThat;
 
 /**
  * Test for ObjectSerializerFactory.
@@ -23,53 +17,6 @@ public class ObjectSerializerFactoryTest {
     @Test(expected = NullPointerException.class)
     public void testCreate() {
         ObjectSerializerFactory.create(null);
-    }
-
-    @Test(expected = RestMockException.class)
-    public void testCreateInvalidContentType() {
-        Person person = new Person("Bob", 20);
-        ObjectSerializeContext ctx = new ObjectSerializeContext(person, MediaType.JPEG.toString());
-        ObjectSerializerFactory.create(ctx);
-    }
-
-    @Test
-    public void testCreateJAXB() {
-        Person person = new Person("Bob", 20);
-        ObjectSerializeContext ctx = new ObjectSerializeContext(person,
-                MediaType.XML_UTF_8.toString());
-        ObjectSerializer os = ObjectSerializerFactory.create(ctx);
-        assertThat(os, notNullValue());
-        assertThat(os, instanceOf(JAXBXmlSerializer.class));
-    }
-
-    @Test
-    public void testCreateJackson() {
-        Person person = new Person("Bob", 20);
-        ObjectSerializeContext ctx = new ObjectSerializeContext(person,
-                MediaType.JSON_UTF_8.toString());
-        ObjectSerializer os = ObjectSerializerFactory.create(ctx);
-        assertThat(os, notNullValue());
-        assertThat(os, instanceOf(JacksonJsonSerializer.class));
-    }
-
-    @Test
-    public void testCreateGson() {
-        Person person = new Person("Bob", 20);
-        ObjectSerializeContext ctx
-                = new ObjectSerializeContext(person, MediaType.JSON_UTF_8.toString()) {
-                    @Override
-                    protected boolean isGsonEnabled() {
-                        return true;
-                    }
-
-                    @Override
-                    protected boolean isJacksonEnabled() {
-                        return false;
-                    }
-                };
-        ObjectSerializer os = ObjectSerializerFactory.create(ctx);
-        assertThat(os, notNullValue());
-        assertThat(os, instanceOf(GsonJsonSerializer.class));
     }
 
 }
