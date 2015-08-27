5aaca5b4a4e3fd6c58be3d1c76b93e654140e5c6
Seiji Sogabe <s.sogabe@gmail.com>
2015-08-27 16:13:15 +0900

    private consstrctor to avoid creating instance.

--- src/main/java/com/buildria/restmock/serialize/ObjectSerializerFactory.java
+++ src/main/java/com/buildria/restmock/serialize/ObjectSerializerFactory.java
@@ -1,8 +1,15 @@
 package com.buildria.restmock.serialize;
 
+import java.util.Objects;
+
 public class ObjectSerializerFactory {
 
+    private ObjectSerializerFactory() {
+        super();
+    }
+
     public static ObjectSerializer create(ObjectSerializeContext ctx) {
+        Objects.requireNonNull(ctx);
         return ctx.createStrategy();
     }
 
