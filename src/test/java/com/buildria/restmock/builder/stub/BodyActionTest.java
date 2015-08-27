a98295e3742b4312dac92b01018aa8551a53f7f8
Seiji Sogabe <s.sogabe@gmail.com>
2015-08-27 21:27:01 +0900

    BodyActionTest

---
+++ src/test/java/com/buildria/restmock/builder/stub/BodyActionTest.java
@@ -0,0 +1,94 @@
+package com.buildria.restmock.builder.stub;
+
+import com.buildria.restmock.TestNameRule;
+import com.buildria.restmock.builder.stub.Action.BodyAction;
+import com.buildria.restmock.builder.stub.Action.HeaderAction;
+import com.buildria.restmock.serialize.Person;
+import com.buildria.restmock.stub.StubHttpServer;
+import com.google.common.base.MoreObjects;
+import io.netty.handler.codec.http.DefaultHttpResponse;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class BodyActionTest {
+
+    @Rule
+    public TestNameRule testNameRule = new TestNameRule();
+
+    private BodyAction target;
+
+    private final Person person = new Person("Bob", 20);
+
+    @Test(expected = NullPointerException.class)
+    public void testConstructorServerNull() throws Exception {
+        StubHttpServer server = null;
+        Matcher<?> uri = equalTo("/api/p");
+        Object content = person;
+        Action action = new BodyAction(server, uri, content);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testConstructorUriNull() throws Exception {
+        StubHttpServer server = new StubHttpServer();
+        Matcher<?> uri = null;
+        Object content = person;
+        Action action = new BodyAction(server, uri, content);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testConstructorContentNull() throws Exception {
+        StubHttpServer server = new StubHttpServer();
+        Matcher<?> uri = equalTo("/api/p");
+        Object content = null;
+        Action action = new BodyAction(server, uri, content);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testApplyResponseNull() throws Exception {
+        StubHttpServer server = new StubHttpServer();
+        Matcher<?> uri = equalTo("/api/p");
+        Object content = person;
+
+        Action action = new BodyAction(server, uri, content);
+        action.apply(null);
+    }
+
+    @Test
+    public void testApplyResponse() throws Exception {
+        StubHttpServer server = new StubHttpServer();
+        server.addAction(new HeaderAction(server, equalTo("/api/p"), "Content-Type", "application/json"));
+        Matcher<?> uri = equalTo("/api/p");
+        Object content = person;
+
+        Action action = new BodyAction(server, uri, content);
+        HttpResponse in = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+        HttpResponse out = action.apply(in);
+
+        assertThat(out, notNullValue());
+        assertThat(out.headers().get("Content-Length"), is("23"));
+    }
+
+    @Test
+    public void testObjects() {
+        StubHttpServer server = new StubHttpServer();
+        Matcher<?> uri = equalTo("/api/p");
+        Object content = person;
+
+        Action action = new BodyAction(server, uri, content);
+
+        MoreObjects.ToStringHelper answer = action.objects();
+        assertThat(answer, notNullValue());
+        assertThat(answer.toString(), containsString("uri"));
+        assertThat(answer.toString(), containsString("content"));
+    }
+}
