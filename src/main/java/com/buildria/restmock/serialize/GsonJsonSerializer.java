d21cdbfeb6712540039e461275cd2f872f376a40
Seiji Sogabe <s.sogabe@gmail.com>
2015-08-27 12:18:52 +0900

    created gson using GsonBuilder

--- src/main/java/com/buildria/restmock/serialize/GsonJsonSerializer.java
+++ src/main/java/com/buildria/restmock/serialize/GsonJsonSerializer.java
@@ -1,6 +1,7 @@
 package com.buildria.restmock.serialize;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -14,7 +15,8 @@ public class GsonJsonSerializer implements ObjectSerializer {
     public String seriaize(ObjectSerializeContext ctx) throws IOException {
         Objects.requireNonNull(ctx);
         Object obj = ctx.getObjectToSerialize();
-        Gson gson = new Gson();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
         return gson.toJson(obj);
     }
 
