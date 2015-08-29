ada85ad8ed577d11e641934763a5f08e75cb774c
Seiji Sogabe <s.sogabe@gmail.com>
2015-08-29 10:20:58 +0900

    use HttpHeaders instead of raw value

--- src/main/java/com/buildria/restmock/builder/verify/RequestSpec.java
+++ src/main/java/com/buildria/restmock/builder/verify/RequestSpec.java
@@ -2,6 +2,7 @@ package com.buildria.restmock.builder.verify;
 
 import com.buildria.restmock.builder.verify.Verifier.Header;
 import com.buildria.restmock.stub.Call;
+import com.google.common.net.HttpHeaders;
 import com.google.common.net.MediaType;
 import java.util.List;
 import org.hamcrest.Matcher;
@@ -33,27 +34,27 @@ public class RequestSpec {
     }
 
     public RequestSpec contentType(Matcher<?> value) {
-        return header("Cotent-Type", value);
+        return header(HttpHeaders.CONTENT_TYPE, value);
     }
 
     public RequestSpec contentType(String value) {
-        return header("Cotent-Type", value);
+        return header(HttpHeaders.CONTENT_TYPE, value);
     }
 
     public RequestSpec contentType(MediaType value) {
-        return header("Cotent-Type", value);
+        return header(HttpHeaders.CONTENT_TYPE, value);
     }
 
     public RequestSpec accept(Matcher<?> value) {
-        return header("Accept", value);
+        return header(HttpHeaders.ACCEPT, value);
     }
 
     public RequestSpec accept(String value) {
-        return header("Accept", value);
+        return header(HttpHeaders.ACCEPT, value);
     }
 
     public RequestSpec accept(MediaType value) {
-        return header("Accept", value);
+        return header(HttpHeaders.ACCEPT, value);
     }
 
 }
