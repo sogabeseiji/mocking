eefe8fedd694eb751d43f85c96316a5ae1aca626
Seiji Sogabe <s.sogabe@gmail.com>
2015-09-01 22:03:37 +0900

    fixed warning

--- src/main/java/com/buildria/restmock/serializer/ObjectSerializer.java
+++ src/main/java/com/buildria/restmock/serializer/ObjectSerializer.java
@@ -3,13 +3,14 @@ package com.buildria.restmock.serializer;
 import java.io.IOException;
 import java.util.Objects;
 
+// CHECKSTYLE:OFF
 public abstract class ObjectSerializer {
+// CHECKSTYLE:ON
 
     private final ObjectSerializerContext ctx;
 
     public ObjectSerializer(ObjectSerializerContext ctx) {
-        Objects.requireNonNull(ctx);
-        this.ctx = ctx;
+        this.ctx = Objects.requireNonNull(ctx);
     }
 
     public ObjectSerializerContext getCtx() {
