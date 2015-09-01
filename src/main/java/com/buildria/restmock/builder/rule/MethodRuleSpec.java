0480874590d002ff62f1407718a24692e8fbb854
Seiji Sogabe <s.sogabe@gmail.com>
2015-09-01 19:20:41 +0900

    Do not create useless instance.

--- src/main/java/com/buildria/restmock/builder/rule/MethodRuleSpec.java
+++ src/main/java/com/buildria/restmock/builder/rule/MethodRuleSpec.java
@@ -4,32 +4,30 @@ import com.buildria.restmock.builder.rule.Rule.Method;
 
 public class MethodRuleSpec extends RuleSpec {
 
-    private static final MethodRuleSpec INSTANCE = new MethodRuleSpec();
-
     private MethodRuleSpec() {
         super();
     }
 
-    private RequestRuleSpec method(String path, String method) {
+    private static RequestRuleSpec method(String path, String method) {
         RequestRuleSpec spec = new RequestRuleSpec();
         spec.addRule(new Method(path, method));
         return spec;
     }
 
     public static RequestRuleSpec get(String path) {
-        return INSTANCE.method(path, "get");
+        return method(path, "get");
     }
 
     public static RequestRuleSpec post(String path) {
-        return INSTANCE.method(path, "post");
+        return method(path, "post");
     }
 
     public static RequestRuleSpec put(String path) {
-        return INSTANCE.method(path, "put");
+        return method(path, "put");
     }
 
     public static RequestRuleSpec delete(String path) {
-        return INSTANCE.method(path, "delete");
+        return method(path, "delete");
     }
 
 }
